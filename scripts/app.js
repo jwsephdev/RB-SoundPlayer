015d72cdbc7e3c6f7003c9e84c0fbfaed59997e4
Jwseph <116055108+jwsephdev@users.noreply.github.com>
2026-04-30 19:35:16 +0800

    keyInputs and some eventListeners

--- scripts/app.js
+++ scripts/app.js
@@ -1,20 +1,29 @@
 const sfx_sample =  new Audio('sample_audio/Pop.mp3');
 const bg_sample =  new Audio('sample_audio/BG_Sample.mp3');
 
+const btn_sfx1 = document.getElementById('sfx1_btn');
+const filename_sfx1 = document.getElementById('sfx1_filename');
+const keybind_sfx1 = document.getElementById('sfx1_keybind');
+
+btn_sfx1.addEventListener("click", () => {
+    playAud(sfx_sample);
+  });
 
 window.addEventListener('keydown', (event) => {
     console.log(('You pressed:'+ event.key));
     if (event.key === 'q') {
-        sfx_sample.currentTime = 0;
-        sfx_sample.play();
+        playAud(sfx_sample);
         
-     }
+     };
     if (event.key === 'w') {
-        bg_sample.currentTime = 0;
-        bg_sample.play();
+        playAud(bg_sample);
     }
     else if (event.key == " ") {
         bg_sample.pause();
     }
-  
 });
+
+function playAud(audio) {
+    audio.currentTime = 0;
+    audio.play();
+};  
